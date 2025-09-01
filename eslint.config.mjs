e463f04f87b5ef107fd685a6950d3d6350899dbb
fbtrtb <broketorichtobroke@gmail.com>
2025-09-01 21:24:53 +0200

    ESlint & Prettier Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,42 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    // "plugin:tailwindcss/recommended",
+    "prettier",
+  ),
+  {
+    rules: {
+      "no-undef": "off",
+    },
+  },
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
+      "components/ui//*",
+      ".next//",
+      "node_modules/**/",
+      "dist//*",
+      "out//*",
       "next-env.d.ts",
+      ".vscode//",
+      ".vercel//",
+      ".git//",
     ],
   },
 ];
 
-export default eslintConfig;
+export default config;
